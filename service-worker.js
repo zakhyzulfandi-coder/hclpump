9671ecbe993c437a8cf80436f85c3895a78cd34e
zakhyzulfandi-coder <zakhyzulfandi@gmail.com>
2026-05-16 14:59:35 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,33 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker Installed');
+const CACHE_NAME = "hcl-pump-v1";
+
+self.addEventListener("install", (event) => {
+
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "/"
+      ]);
+    })
+  );
+
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("activate", (event) => {
+
+  event.waitUntil(
+    clients.claim()
+  );
+
+});
+
+self.addEventListener("fetch", (event) => {
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+
 });
